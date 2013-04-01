36841fd25da7414fb7f3fe14b9dd0ed8a3412f48
ben <ben@yuteki-Mac-mini.local>
2013-04-01 10:57:48 +0800

    commit test 1

---
+++ test/testViewController.h
@@ -0,0 +1,13 @@
+//
+//  testViewController.h
+//  test
+//
+//  Created by ben on 13/4/1.
+//  Copyright (c) 2013年 huntech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testViewController : UIViewController
+
+@end
---
+++ test/testViewController.m
@@ -0,0 +1,43 @@
+//
+//  testViewController.m
+//  test
+//
+//  Created by ben on 13/4/1.
+//  Copyright (c) 2013年 huntech. All rights reserved.
+//
+
+#import "testViewController.h"
+
+@interface testViewController ()
+
+@end
+
+@implementation testViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
